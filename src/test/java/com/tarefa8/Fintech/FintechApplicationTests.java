151fa9d3dbb1efd1b30ac5e4de1bb965c2532284
gabriel <gabrielsartarelli@outlook.com>
2023-10-20 17:03:53 -0300

    first commit configurações iniciais

---
+++ src/test/java/com/tarefa8/Fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefa8.Fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
